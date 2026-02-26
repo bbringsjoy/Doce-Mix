f2379ab3fb4bc8a89e0cb30fc08083e8592c4e8a
Beatriz <biagsantana1000@gmail.com>
2026-02-25 21:52:41 -0300

    Instalando react e começando o front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Header from "./componentes/Header";
+
+function App(){
+  return(
+    <div>
+     
+
+      <Header />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/componentes/Header.js
@@ -0,0 +1,19 @@
+function Header(){
+  return(
+    <header className="header" >
+
+      <div >
+         <h1>Loja de Impressão 3D</h1>
+      <p>A loja será aberta em breve!</p>
+      </div>
+      <nav className="menu_header">
+          <a href='#'>Home</a>
+          <a href='#'>Produtos</a>
+          <a href='#'>Contato</a>
+          <a href='#'>Sobre</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
